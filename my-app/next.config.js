b26db87c251aea0aef1bebf8d1162f6c15d821ee
2yunseong <dbsdltjd3701@naver.com>
2024-01-15 17:03:06 +0900

    next.js 프로젝트 세팅

---
+++ my-app/next.config.js
@@ -0,0 +1,8 @@
+// 타입스크립트를 위한 주석
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // swcMinify: true -> v13이후로 기본 설정으로 바뀜
+};
+
+module.exports = nextConfig;
